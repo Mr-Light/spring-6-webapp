ccb8d5e61dd2e7c5a57a4e813110426d91024e6c
a_hawaryy <adham.elhawary1@gmail.com>
2024-11-11 18:07:29 +0200

    Renamed package bemazagy

--- src/main/java/learn/doomispring/Spring6WebappApplication.java
+++ src/main/java/learn/doomispring/Spring6WebappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring6webapp;
+package learn.doomispring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

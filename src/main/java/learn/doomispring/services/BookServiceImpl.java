b77575e5cc3130628d739b0c7b517578ecdadef6
a_hawaryy <adham.elhawary1@gmail.com>
2024-11-12 17:52:29 +0200

    added service annotation

--- src/main/java/learn/doomispring/services/BookServiceImpl.java
+++ src/main/java/learn/doomispring/services/BookServiceImpl.java
@@ -2,7 +2,9 @@ package learn.doomispring.services;
 
 import learn.doomispring.domain.Book;
 import learn.doomispring.repositories.BookRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
 
     private BookRepository bookRepository;
